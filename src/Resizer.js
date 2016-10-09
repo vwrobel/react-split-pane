a6fd0fe0f78a3f6a7a49092bbbbc4a8a01da1adb
vwrobel <valerian.wrobel1@gmail.com>
2016-10-09 10:34:55 +0200

    no prefixer

--- src/Resizer.js
+++ src/Resizer.js
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react';
-import prefix from 'react-prefixer';
 
 class Resizer extends Component {
 
@@ -9,7 +8,7 @@ class Resizer extends Component {
         return (
             <span
               className={classes.join(' ')}
-              style={prefix(this.props.style) || {}}
+              style={this.props.style || {}}
               onMouseDown={(event) => {
                 this.props.onMouseDown(event);
               }}
